faf143b8eacb6cd120e479a5cebf35c3a300dfbf
Lobsang Lama <lobby@Lobsangs-MacBook-Air.local>
2024-10-04 23:59:28 +0100

    completed companion list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,16 +2,45 @@ import { SearchInput } from '@/components/search-input';
 
 import prisma from '@/lib/prismadb';
 import { Categories } from '@/components/categories';
+import Companions from '@/components/companions';
+
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
 
 // this is server component and we're going to be using the library file prismadb.ts to use it here
-const RootPage = async () => {
-    const categories = await prisma.category.findMany();
+const RootPage = async ({
+    searchParams
+}: RootPageProps) => {
+    const data = await prisma.companion.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    });
 
+    const categories = await prisma.category.findMany();
 
     return ( 
         <div className= "h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
      );
 }
